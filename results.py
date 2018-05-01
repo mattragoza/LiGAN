1ef672450cf311535f144187db3dd6b7a7557aa3
Matt Ragoza <mtr22@pitt.edu>
2018-05-01 14:39:40 -0400

    specify x, y, hue as command line args to results.py

--- results.py
+++ results.py
@@ -16,7 +16,6 @@ sns.set_style('white')
 sns.set_context('talk')
 
 import models
-import generate
 
 
 def plot_lines(plot_file, df, x, y, hue, ylim=None):
@@ -31,25 +30,32 @@ def plot_lines(plot_file, df, x, y, hue, ylim=None):
         ax.plot(s.index, s, label=i, linewidth=1.0)
     fig.tight_layout()
     lgd = ax.legend(loc='upper left', bbox_to_anchor=(1.00, 1.025), ncol=2)
+    lgd.set_title(hue, prop=dict(size='small'))
     fig.savefig(plot_file, bbox_extra_artists=(lgd,), bbox_inches='tight')
 
 
-def plot_strips(plot_file, df, xs, y, hue, ylim=None, n_cols=4):
+def plot_strips(plot_file, df, x, y, hue, ylim=None, n_cols=None):
     df = df.reset_index()
-    n_x = len(xs)
-    n_rows = (n_x + n_cols-1)//n_cols
-    n_cols = min(n_x, n_cols)
-    fig, axes = plt.subplots(n_rows, n_cols, figsize=(4*n_cols, 4*n_rows), sharey=True)
+    if n_cols is None:
+        n_cols = len(x)
+    n_ax = len(x)*len(y)
+    n_rows = (n_ax + n_cols-1)//n_cols
+    n_cols = min(n_ax, n_cols)
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(4*n_cols, 4*n_rows),
+                             sharey=len(y) == 1, squeeze=False)
+    axes = axes.reshape(n_rows, n_cols)
     if ylim:
         axes[0][0].set_ylim(*ylim)
-    axes = axes.flatten()
-    for i, x in enumerate(xs):
-        ax = axes[i]
-        sns.stripplot(data=df, x=x, y=y, hue=hue, jitter=True, ax=ax)
-        handles, labels = ax.get_legend_handles_labels()
-        ax.legend(handles, labels, ncol=1)
-    for i in range(i+1, len(axes)):
-        axes[i].axis('off')
+    axes = iter(axes.flatten())
+    for i, y_ in enumerate(y):
+        for j, x_ in enumerate(x):
+            ax = next(axes)
+            sns.stripplot(data=df, x=x_, y=y_, hue=hue, jitter=True, ax=ax)
+            handles, labels = ax.get_legend_handles_labels()
+            lgd = ax.legend(handles, labels, ncol=1)
+            lgd.set_title(hue, prop=dict(size='small'))
+    for ax in axes:
+        ax.axis('off')
     fig.tight_layout()
     fig.savefig(plot_file)
 
@@ -96,19 +102,18 @@ def parse_args(argv=None):
     parser.add_argument('-i', '--iteration', default=20000, type=int)
     parser.add_argument('--plot_ext', default='png')
     parser.add_argument('-r', '--rename_col', default=[], action='append')
+    parser.add_argument('-x', '--x', default=[], action='append')
+    parser.add_argument('-y', '--y', default=[], action='append')
+    parser.add_argument('--hue', default=None)
     return parser.parse_args(argv)
 
 
-if __name__ == '__main__':
-    argv = sys.argv[1:]
+def main(argv):
     args = parse_args(argv)
 
     model_dirs = sorted(d for d in glob.glob(args.dir_pattern) if os.path.isdir(d))
     seeds = map(int, args.seeds.split(','))
     folds = map(int, args.folds.split(','))
-
-    model_version = tuple(int(c) for c in re.match(r'^.+e(\d+)_', args.dir_pattern).group(1))
-    name_format = models.NAME_FORMATS[model_version]
 
     df = read_training_output_files(model_dirs, args.data_name, seeds, folds, args.iteration)
 
@@ -123,18 +128,51 @@ if __name__ == '__main__':
     assert np.all(agg_df['fold'] == len(folds))
 
     for model_name, model_df in agg_df.groupby(level=0):
+        model_version = tuple(int(c) for c in re.match(r'^.+e(\d+)_', model_name).group(1))
+        agg_df.loc[model_name, 'model_version'] = str(model_version)
+        name_format = models.NAME_FORMATS[model_version]
         name_parse = parse.parse(name_format, model_name)
         name_fields = sorted(name_parse.named, key=name_parse.spans.get)
         for field in name_fields:
-            agg_df.loc[model_name, field] = name_parse.named[field]
+            value = name_parse.named[field]
+            agg_df.loc[model_name, field] = value
 
-    col = col_name_map.get('test_y_loss', 'test_y_loss')
+    name_fields.insert(0, 'model_version')
+
+    test_loss = col_name_map.get('test_y_loss', 'test_y_loss')
+    train_loss = col_name_map.get('train_y_loss', 'train_y_loss')
+
+    if 'resolution' in agg_df:
+        agg_df['resolution'] = agg_df['resolution'].fillna(0.5)
+        test_loss_res = test_loss + '*resolution^3'
+        train_loss_res = train_loss + '*resolution^3'
+        agg_df[test_loss_res] = agg_df[test_loss]*agg_df['resolution']**3
+        agg_df[train_loss_res] = agg_df[train_loss]*agg_df['resolution']**3
+
+    if 'conv_per_level' in agg_df:
+        agg_df = agg_df[agg_df['conv_per_level'] > 0]
+
+    if 'width_factor' in agg_df:
+        agg_df['width_factor'] = agg_df['width_factor'].fillna(1)
+        agg_df = agg_df[agg_df['width_factor'] < 3]
+
+    if 'n_latent' in agg_df:
+        agg_df['n_latent'] = agg_df['n_latent'].fillna(0)
+
+    if 'loss_types' in agg_df:
+        agg_df['loss_types'] = agg_df['loss_types'].fillna('e')
+        agg_df = agg_df[agg_df['loss_types'] == 'e']
 
     # plot training progress
-    line_plot_file = '{}_{}_lines.{}'.format(args.out_prefix, col, args.plot_ext)
-    plot_lines(line_plot_file, agg_df, x='iteration', y=col, hue='model_name')
+    line_plot_file = '{}_{}_lines.{}'.format(args.out_prefix, test_loss, args.plot_ext)
+    plot_lines(line_plot_file, agg_df, x='iteration', y=test_loss, hue='model_name')
 
     # plot final loss distributions
     agg_df.reset_index('model_name', inplace=True)
-    strip_plot_file = '{}_{}_strips.{}'.format(args.out_prefix, col, args.plot_ext)
-    plot_strips(strip_plot_file, agg_df.loc[args.iteration], xs=name_fields, y=col, hue='n_filters')
+    strip_plot_file = '{}_strips.{}'.format(args.out_prefix, args.plot_ext)
+    plot_strips(strip_plot_file, agg_df.loc[args.iteration], x=args.x or name_fields,
+                y=args.y or [test_loss], hue=args.hue, n_cols=4)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
