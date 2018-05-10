6378e57fd46417afbd6841408bec1f61d2fd8755
Matt Ragoza <mtr22@pitt.edu>
2018-05-10 10:11:04 -0400

    submit jobs by monitoring n free gpus in run_experiment.py

--- run_experiment.py
+++ run_experiment.py
@@ -1,4 +1,5 @@
 import sys, os, glob, time
+import numpy as np
 import shlex
 import subprocess as sp
 import multiprocessing as mp
@@ -36,11 +37,11 @@ def get_job_state(job_id):
     cmd = 'qstat -f -t {}'.format(job_id)
     stdout, stderr = run_subprocess(cmd)
     if stderr:
-        raise Exception(stderr)
+        return 'C' #ignore unknown job id
     return parse_qstat(stdout).loc[job_id]['job_state']
 
 
-def run_queue_job(args):
+def submit_job_and_wait_to_complete(args):
     pbs_file, array_idx = args
     work_dir, pbs_file = os.path.split(pbs_file)
     if work_dir:
@@ -54,13 +55,52 @@ def run_queue_job(args):
         os.chdir(orig_dir)
 
 
+def get_n_gpus_free(queue):
+    cmd = 'pbsnodes'
+    stdout, stderr = run_subprocess(cmd)
+    if stderr:
+        raise Exception(stderr)
+    df = parse_qstat(stdout)
+    df = df[df['properties'].map(lambda qs: queue in qs.split(','))]
+    df = df[df['state'] == 'free']
+    n_gpus = df['gpus'].astype(int)
+    cmd = 'qstat -f -t'
+    stdout, stderr = run_subprocess(cmd)
+    if stderr:
+        raise Exception(stderr)
+    df = parse_qstat(stdout)
+    df = df[df['job_state'] == 'R']
+    df['Node Id'] = df['gpus_reserved'].map(lambda g: g.split(':', 1)[0])
+    df['gpu'] = df['gpus_reserved'].map(lambda g: g.split(':', 1)[1])
+    df = df[df['Node Id'].map(lambda n: n in n_gpus)]
+    n_gpus_used = df.groupby('Node Id')['gpu'].nunique().astype(int)
+    n_gpus_free = n_gpus.subtract(n_gpus_used, fill_value=0).astype(int)
+    return n_gpus_free.sum()
+
+
+def wait_for_free_gpus_and_submit_job(args):
+    pbs_file, array_idx = args
+    work_dir, pbs_file = os.path.split(pbs_file)
+    if work_dir:
+        orig_dir = os.getcwd()
+        os.chdir(work_dir)
+    while get_n_gpus_free(queue='dept_gpu') < 4:
+        time.sleep(5)
+    job_id = submit_job(pbs_file, array_idx)
+    print(job_id)
+    time.sleep(5)
+    if work_dir:
+        os.chdir(orig_dir)
+
+
 if __name__ == '__main__':
-    pbs_template = 'train2.pbs'
+    pbs_template = 'masked.pbs'
     #model_files = [line.rstrip() for line in open('memory_error_models')]
-    #model_files = glob.glob('models/*e13*.model')
-    df = parse_qstat(open('qjobs').read())
-    model_names = df[(df['euser'] == 'mtr22') & (df['job_state'] == 'Q')]['Job_Name']
-    model_files = ['models/' + m + '.model' for m in model_names]
+    model_files = glob.glob('models/*e13*_e.model')
+    #df = parse_qstat(open('qjobs').read())
+    #model_names = df[(df['euser'] == 'mtr22') & (df['job_state'] == 'Q')]['Job_Name']
+    #model_names = [m for m in model_names if not len(glob.glob(m + '/' + m + '_iter_20000.caffemodel')) == 4]
+    #model_files = ['models/' + m + '.model' for m in model_names]
     for model_file in model_files:
         assert os.path.isfile(model_file), 'file {} does not exist'.format(model_file)
 
@@ -69,7 +109,6 @@ if __name__ == '__main__':
     iterations = 20000
     seeds = [0]
     folds = [0, 1, 2, 3]
-    n_processes = 20
 
     args = []
     for model_file in model_files:
@@ -82,7 +121,5 @@ if __name__ == '__main__':
                 write_pbs_file(pbs_file, pbs_template, model_name, data_name, data_root, iterations)
                 args.append((pbs_file, 4*seed + fold))
 
-    pool = mp.Pool(n_processes)
-    pool.map(run_queue_job, args)
-    pool.terminate()
+    map(wait_for_free_gpus_and_submit_job, args)
 
