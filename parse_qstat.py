95d0a255a03447f84fb198988e52a1b64945dee0
Matt Ragoza <mtr22@pitt.edu>
2018-05-10 10:10:05 -0400

    parse_qstat() can now handle pbsnodes output

--- parse_qstat.py
+++ parse_qstat.py
@@ -4,14 +4,24 @@ import pandas as pd
 
 
 def parse_qstat(buf):
+    assert buf, 'nothing to parse'
+    pbsnodes = not buf.startswith('Job Id')
     all_job_data = []
-    for job_buf in filter(len, buf.split('\n\n')):
+    job_delim = '\n\n'
+    for job_buf in filter(len, buf.split(job_delim)):
         job_data = dict()
-        for field_buf in filter(len, job_buf.split('\n    ')):
-            name, value = field_buf.split(': ' if not job_data else ' = ', 1)
+        field_delim = '\n' + (4, 5)[pbsnodes]*' '
+        for field_buf in filter(len, job_buf.split(field_delim)):
+            if not job_data:
+                if pbsnodes:
+                    name, value = 'Node Id', field_buf
+                else:
+                    name, value = field_buf.split(': ', 1)
+            else:
+                name, value = field_buf.split(' = ', 1)
             job_data[name] = value.replace('\n\t', '')
         all_job_data.append(job_data)
-    return pd.DataFrame(all_job_data).set_index('Job Id')
+    return pd.DataFrame(all_job_data).set_index('Node Id' if pbsnodes else 'Job Id')
 
 
 if __name__ == '__main__':
