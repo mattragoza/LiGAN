0890fe08fdd085b3e174bbcf3ec96ff0f3baffb2
Matt Ragoza <mtr22@pitt.edu>
2018-07-31 11:03:38 -0400

    pass more customizable params files to run_experiment.py, add docstrings and print n_gpus_free

--- run_experiment.py
+++ run_experiment.py
@@ -11,6 +11,11 @@ MIN_GPUS_FREE = 4
 
 
 def write_pbs_file(pbs_file, pbs_template_file, job_name, **kwargs):
+    '''
+    Make a copy of a pbs job script file that has -n JOB_NAME replaced
+    with job_name and any variable definitions whose names are all-
+    caps versions of keys in kwargs redefined as the arg value.
+    '''
     with open(pbs_template_file) as f:
         buf = f.read()
     buf = re.sub(r'#PBS -N JOB_NAME', '#PBS -N {}'.format(job_name), buf)
@@ -22,12 +27,18 @@ def write_pbs_file(pbs_file, pbs_template_file, job_name, **kwargs):
 
 
 def run_subprocess(cmd, stdin=None):
+    '''
+    Run a subprocess with the given stdin and return (stdout, stderr).
+    '''
     args = shlex.split(cmd)
     proc = sp.Popen(args, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
     return proc.communicate(stdin)
 
 
 def submit_job(pbs_file, array_idx):
+    '''
+    Submit a job script with an array index using qsub.
+    '''
     cmd = 'qsub {} -t {}'.format(pbs_file, array_idx)
     stdout, stderr = run_subprocess(cmd)
     if stderr:
@@ -36,6 +47,9 @@ def submit_job(pbs_file, array_idx):
 
 
 def get_job_state(job_id):
+    '''
+    Query the state of a job with the given job_id using qstat.
+    '''
     cmd = 'qstat -f -t {}'.format(job_id)
     stdout, stderr = run_subprocess(cmd)
     if stderr:
@@ -44,6 +58,11 @@ def get_job_state(job_id):
 
 
 def submit_job_and_wait_to_complete(args):
+    '''
+    Submit a job with the args (pbs_file, array_idx) and then wait
+    for it to complete, querying its state every 5 seconds. The job
+    will be submit from the dir containing the job script.
+    '''
     pbs_file, array_idx = args
     work_dir, pbs_file = os.path.split(pbs_file)
     if work_dir:
@@ -58,6 +77,11 @@ def submit_job_and_wait_to_complete(args):
 
 
 def get_n_gpus_free(queue):
+    '''
+    Compute the number of gpus in a particular job queue that are
+    not currently being used by any jobs, using pbsnodes and qstat.
+    '''
+    # get total n_gpus in each currently available node in the queue
     cmd = 'pbsnodes'
     stdout, stderr = run_subprocess(cmd)
     if stderr:
@@ -66,32 +90,43 @@ def get_n_gpus_free(queue):
     df = df[df['properties'].map(lambda qs: queue in qs.split(','))]
     df = df[df['state'] == 'free']
     n_gpus = df['gpus'].astype(int)
+    # get jobs running on those nodes that are using gpus
     cmd = 'qstat -f -t'
     stdout, stderr = run_subprocess(cmd)
     if stderr:
         raise Exception(stderr)
     df = parse_qstat(stdout)
     df = df[df['job_state'] == 'R']
-    try:
-        df['Node Id'] = df['gpus_reserved'].map(lambda g: g.split(':', 1)[0])
-    except AttributeError:
-        print(df['gpus_reserved'][df['gpus_reserved'].map(lambda g: type(g) == float)])
-        raise
-    df['gpu'] = df['gpus_reserved'].map(lambda g: g.split(':', 1)[1])
+    df = df[df['gpus_reserved'].notnull()]
+    df['Node Id'] = df['gpus_reserved'].map(lambda g: g.split(':', 1)[0])
+    df['gpu']     = df['gpus_reserved'].map(lambda g: g.split(':', 1)[1])
     df = df[df['Node Id'].map(lambda n: n in n_gpus)]
+    # get num unique gpus being used by those jobs on each node
     n_gpus_used = df.groupby('Node Id')['gpu'].nunique().astype(int)
+    # return total free gpus as sum of total gpus minus gpus in use, per node
     n_gpus_free = n_gpus.subtract(n_gpus_used, fill_value=0).astype(int)
     return n_gpus_free.sum()
 
 
 def wait_for_free_gpus_and_submit_job(args):
+    '''
+    Wait for a certain number of gpus to be free and then
+    submit a job with args (pbs_file, array_idx). The job
+    will be submit from the dir containing the job script.
+    '''
     pbs_file, array_idx = args
     work_dir, pbs_file = os.path.split(pbs_file)
     if work_dir:
         orig_dir = os.getcwd()
         os.chdir(work_dir)
-    while get_n_gpus_free(queue='dept_gpu') <= MIN_GPUS_FREE:
+    queue = 'dept_gpu'
+    last_n_gpus_free = -1
+    n_gpus_free = get_n_gpus_free(queue)
+    while n_gpus_free <= MIN_GPUS_FREE:
+        if n_gpus_free != last_n_gpus_free:
+            print(n_gpus_free)
         time.sleep(5)
+        n_gpus_free = get_n_gpus_free(queue)
     job_id = submit_job(pbs_file, array_idx)
     print(job_id)
     time.sleep(5)
@@ -101,32 +136,26 @@ def wait_for_free_gpus_and_submit_job(args):
 
 if __name__ == '__main__':
 
-    _, models_file = sys.argv
+    _, params_file = sys.argv
     pbs_templates = ['bgan.pbs', 'agan.pbs', 'abgan.pbs']
-
-    model_names = [line.rstrip() for line in open(models_file)]
-    model_files = ['models/' + m + '.model' for m in model_names]
-    for model_file in model_files:
-        assert os.path.isfile(model_file), 'file {} does not exist'.format(model_file)
+    params = [line.rstrip().split() for line in open(params_file)]
 
     data_name = 'lowrmsd' #'genlowrmsd'
     data_root = '/net/pulsar/home/koes/dkoes/PDBbind/refined-set/' #general-set-with-refined/'
     max_iter = 20000
     cont_iter = 0
-    seeds = [0]
-    folds = [0, 1, 2, 3]
 
     args = []
-    for pbs_template, model_file, seed, fold in itertools.product(pbs_templates, model_files, seeds, folds):
-        model_name = os.path.splitext(os.path.split(model_file)[1])[0]
+    for pbs_template, gen_model_file, disc_model_file, seed, fold in params:
+        seed, fold = int(seed), int(fold)
+        gen_model_name = os.path.splitext(os.path.split(gen_model_file)[1])[0]
         if 'gan' in pbs_template:
             gan_type = os.path.splitext(os.path.basename(pbs_template))[0]
-            resolution = model_name.split('_')[3]
-            gen_model_name = model_name
+            resolution = gen_model_name.split('_')[3]
             data_model_name = 'data_24_{}'.format(resolution)
-            disc_model_name = 'disc' #'info_2048' if '1024' in model_name else 'info_1024'
+            disc_model_name = os.path.splitext(os.path.split(disc_model_file)[1])[0]
             solver_name = 'adam0'
-            gen_warmup_name = model_name.lstrip('_')
+            gen_warmup_name = gen_model_name.lstrip('_')
             gan_name = '{}{}_{}'.format(gan_type, gen_model_name, disc_model_name)
             if not os.path.isdir(gan_name):
                 os.makedirs(gan_name)
@@ -143,11 +172,11 @@ if __name__ == '__main__':
                            cont_iter=cont_iter,
                            gen_warmup_name=gen_warmup_name)
         else:
-            if not os.path.isdir(model_name):
-                os.makedirs(model_name)
-            pbs_file = os.path.join(model_name, pbs_template)
-            write_pbs_file(pbs_file, pbs_template, model_name,
-                           model_name=model_name,
+            if not os.path.isdir(gen_model_name):
+                os.makedirs(gen_model_name)
+            pbs_file = os.path.join(gen_model_name, pbs_template)
+            write_pbs_file(pbs_file, pbs_template, gen_model_name,
+                           model_name=gen_model_name,
                            data_name=data_name,
                            data_root=data_root,
                            max_iter=max_iter)
