3b67a7f0826cd9abdfe8b747b2b6881dbcc9e6c1
Matt Ragoza <mtr22@pitt.edu>
2018-09-07 15:43:27 -0400

    evaluate ce models on toy problem

--- gan_experiment.py
+++ gan_experiment.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     #_, params_file = sys.argv
     #params = [line.rstrip().split() for line in open(params_file)]
 
-    data_name = 'lowrmsd' #'genlowrmsd'
-    data_root = '/net/pulsar/home/koes/dkoes/PDBbind/refined-set/' #general-set-with-refined/'
-    max_iter = 100000
+    data_name = 'two_atoms' #'lowrmsd' #'genlowrmsd'
+    data_root = '/net/pulsar/home/koes/mtr22/gan/data/' #'/net/pulsar/home/koes/dkoes/PDBbind/refined-set/' #general-set-with-refined/'
+    max_iter = 10000
     cont_iter = 0
     seed = 0
 
@@ -18,11 +18,11 @@ if __name__ == '__main__':
     for pbs_template in ['adam0_2_2_b_0.0.pbs', 'adam0_2_2_b_0.1.pbs']:
         for gen_model_file in glob.glob('models/_vr-le14_24_*'):
             for disc_model_file in glob.glob('models/disc2_in*'):
-                for fold in range(4):
+                for fold in [3]:
                     gan_type = os.path.splitext(os.path.basename(pbs_template))[0]
                     gen_model_name = os.path.splitext(os.path.split(gen_model_file)[1])[0]
                     resolution = 0.5 #gen_model_name.split('_')[3]
-                    data_model_name = 'data_24_{}_cov'.format(resolution)
+                    data_model_name = 'data_24_{}_cov_origin'.format(resolution)
                     disc_model_name = os.path.splitext(os.path.split(disc_model_file)[1])[0]
                     seed, fold = int(seed), int(fold)
                     gen_warmup_name = gen_model_name.lstrip('_')
