c46d5225108dff0eb518a510e3ed13618c5d8800
Matt Ragoza <mtr22@pitt.edu>
2019-02-27 12:08:31 -0500

    slurm support wip

---
+++ job_templates/slurm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=GAN_NAME
+#SBATCH --gres=gpu:1
+#SBATCH --mem=MEMORY
+#SBATCH --time=WALLTIME
+#SBATCH --qos=normal
+#SBATCH --cluster=gpu
+#SBATCH --partition=QUEUE
+
+source ~/.bashrc
+cd ${SLURM_SUBMIT_DIR}
+
+SEED=$((${SLURM_ARRAY_TASK_ID} / 4))
+FOLD=$((${SLURM_ARRAY_TASK_ID} % 4))
+
+if (($FOLD == 3)); then
+	FOLD=all;
+fi
+
+cp ${LIGAN_ROOT}/data/DATA_PREFIX*.types ${SLURM_SCRATCH}
+cp ${LIGAN_ROOT}/models/DATA_MODEL_NAME.model ${SLURM_SCRATCH}
+cp ${LIGAN_ROOT}/models/GEN_MODEL_NAME.model ${SLURM_SCRATCH}
+cp ${LIGAN_ROOT}/models/DISC_MODEL_NAME.model ${SLURM_SCRATCH}
+cp ${LIGAN_ROOT}/solvers/SOLVER_NAME.solver ${SLURM_SCRATCH}
+cp ${LIGAN_ROOT}/weights/lig_gauss_conv.caffemodel ${SLURM_SCRATCH}
+
+if ((CONT_ITER > 0)); then
+	cp GAN_NAME.*_iter_CONT_ITER.solverstate ${SLURM_SCRATCH}
+	cp GAN_NAME.*_iter_CONT_ITER.caffemodel ${SLURM_SCRATCH}
+fi
+
+cd ${SLURM_SCRATCH}
+
+trap "cp *.{model,solver,caffemodel,solverstate,training_output,png,pdf} ${SLURM_SUBMIT_DIR}" EXIT
+
+python ${LIGAN_ROOT}/train.py -d DATA_MODEL_NAME.model -g GEN_MODEL_NAME.model -a DISC_MODEL_NAME.model -p DATA_PREFIX -r DATA_ROOT -n ${FOLD} --random_seed ${SEED} -s SOLVER_NAME.solver --max_iter MAX_ITER --cont_iter CONT_ITER --gen_train_iter GEN_TRAIN_ITER --disc_train_iter DISC_TRAIN_ITER --instance_noise INSTANCE_NOISE --loss_weight LOSS_WEIGHT --loss_weight_decay LOSS_WEIGHT_DECAY TRAIN_OPTIONS -o GAN_NAME.DATA_PREFIX.${SEED}
+
+exit
