a99e221097c09c63923981bdae631ef60354f6d6
Matt Ragoza <mtr22@pitt.edu>
2020-07-01 16:58:48 -0400

    handle train jobs in job_errrors.py, prelim

--- job_errors.py
+++ job_errors.py
@@ -51,10 +51,62 @@ def match_files_in_dir(dir, pattern):
             yield m
 
 
+def find_job_ids(job_dir, stderr_pat):
+    '''
+    Find job ids that have been submitted by
+    parsing stderr file names in job_dir.
+    '''
+    job_ids = []
+    for m in match_files_in_dir(job_dir, stderr_pat):
+        job_id = int(m.group(1))
+        job_ids.append(job_id)
+
+    return job_ids
+
+
+def read_job_output(job_dir, output_pat):
+    '''
+    Find job ids that have been submitted by
+    parsing stderr file names in job_dir.
+    '''
+    job_dfs = []
+    for m in match_files_in_dir(job_dir, output_pat):
+        output_file = os.path.join(job_dir, m.group(0))
+        job_df = pd.read_csv(output_file, sep=' ')
+        job_df['job_name']  = os.path.split(job_dir)[-1]
+        try:
+            array_idx = int(m.group(2))
+            job_df['array_idx'] = array_idx
+        except:
+            pass
+        job_dfs.append(job_df)
+
+    return job_dfs
+
+
+def print_last_error(job_dir, stderr_pat):
+
+    last_job_id = -1
+    last_stderr_file = None
+    for m in match_files_in_dir(job_dir, stderr_pat):
+        stderr_file = os.path.join(job_dir, m.group(0))
+        job_id = int(m.group(1))
+        if job_id > last_job_id:
+            last_job_id = job_id
+            last_stderr_file = stderr_file
+
+    if last_stderr_file is None:
+        print('no error file')
+        return
+
+    error = read_stderr_file(last_stderr_file)
+    print(last_stderr_file + '\t' + str(error))
+
+
 def find_submitted_array_indices(job_dir, stderr_pat):
     '''
     Find array indices and job ids that have been
-    submitted by parsing stderr files in job_dir.
+    submitted by parsing stderr file names in job_dir.
     '''
     submitted = set()
     job_ids = []
@@ -68,15 +120,15 @@ def find_submitted_array_indices(job_dir, stderr_pat):
     return submitted, job_ids
 
 
-def copy_back_from_scr_dir(job_dir, scr_dir, output_pat):
+def copy_back_from_scr_dir(job_dir, scr_dir, copy_back_pat):
     '''
     Copy back output files from scr_dir to job_dir.
     '''
     copied = []
-    for m in match_files_in_dir(scr_dir, output_pat):
-        output_file = m.group(0)
-        src_file = os.path.join(scr_dir, output_file)
-        dst_file = os.path.join(job_dir, output_file)
+    for m in match_files_in_dir(scr_dir, copy_back_pat):
+        copy_back_file = m.group(0)
+        src_file = os.path.join(scr_dir, copy_back_file)
+        dst_file = os.path.join(job_dir, copy_back_file)
         shutil.copyfile(src_file, dst_file)
         copied.append(dst_file)
 
@@ -126,6 +178,8 @@ def print_errors_for_array_indices(job_dir, stderr_pat, indices):
 def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('job_dirs', nargs='+')
+    parser.add_argument('--job_type', required=True)
+    parser.add_argument('--array_job', default=False, action='store_true')
     parser.add_argument('--copy_back', '-c', default=False, action='store_true')
     parser.add_argument('--print_indices', '-i', default=False, action='store_true')
     parser.add_argument('--print_errors', '-e', default=False, action='store_true')
@@ -137,65 +191,100 @@ def parse_args(argv):
 def main(argv):
     args = parse_args(argv)
 
+    assert args.job_type in {'train', 'fit'}
+
     for job_dir in args.job_dirs:
         assert os.path.isdir(job_dir), job_dir + ' is not a directory'
 
-    output_pat = re.compile(r'(.*)_(\d+)\.gen_metrics')
-    stderr_pat = re.compile(r'slurm-(\d+)_(\d+)\.err')
-    inf = float('inf')
+    if args.job_type == 'train':
+        output_ext = 'training_output'
+        copy_back_exts = [
+            'model', 'solver','caffemodel', 'solverstate', 'training_output', 'png', 'pdf'
+        ]
 
-    job_dfs = []
+    elif args.job_type == 'fit':
+        output_ext = 'gen_metrics'
+        copy_back_exts = [
+            'types', 'model', 'caffemodel', 'dx', 'sdf', 'pymol', 'gen_metrics'
+        ]
+
+    if args.array_job:
+        stderr_pat = re.compile(r'slurm-(\d+)_(\d+)\.err$')
+        output_pat = re.compile(r'(.*)_(\d+)\.' + output_ext + '$')
+        copy_back_pat = re.compile(r'(.*)_(\d+)\.' + '({})$'.format('|'.join(copy_back_exts)))
+
+    else:
+        stderr_pat = re.compile(r'slurm-(\d+)\.err$')
+        output_pat = re.compile(r'(.*)\.' + output_ext + '$')
+        copy_back_pat = re.compile(r'(.*)\.' + '({})$'.format('|'.join(copy_back_exts)))
+
+    all_job_dfs = []
     for job_dir in args.job_dirs:
+
         print(job_dir)
 
-        submitted, job_ids = find_submitted_array_indices(job_dir, stderr_pat)
+        if args.array_job:
 
-        n_submitted = len(submitted)
-        print('n_submitted = {}'.format(n_submitted))
+            submitted, job_ids = find_submitted_array_indices(job_dir, stderr_pat)
+            n_submitted = len(submitted)
+            
+            if n_submitted == 0:
+                print('none submitted')
+                continue
+            
+            completed, job_dfs = find_completed_array_indices(job_dir, output_pat, read=args.output_file)
+            n_completed = len(completed)
 
-        if n_submitted == 0:
-            continue
+            if args.output_file:
+                all_job_dfs.extend(job_dfs)
 
-        if args.print_indices:
-            print_array_indices(submitted)
+            incomplete = submitted - completed
+            n_incomplete = len(incomplete)
 
-        if args.copy_back:
-            last_job_id = sorted(job_ids)[-1]
-            scr_dir = job_dir + '/' + str(last_job_id)
+            print('n_submitted = {}'.format(n_submitted, get_array_indices_string(submitted)))
+            print('n_completed = {}'.format(n_completed, get_array_indices_string(completed)))
+            print('n_incomplete = {}'.format(n_incomplete, get_array_indices_string(incomplete)))
 
-            copied = copy_back_from_scr_dir(job_dir, scr_dir, output_pat)
+            if args.print_errors:
+                print_errors_for_array_indices(job_dir, stderr_pat, indices=incomplete)
 
-            n_copied = len(copied)
-            print('copied {} files from {}'.format(n_copied, last_job_id))
+            if args.copy_back:
 
-        completed, job_dfs = find_completed_array_indices(job_dir, output_pat, read=args.output_file)
+                last_job_id = sorted(job_ids)[-1]
+                scr_dir = os.path.join(job_dir, str(last_job_id))
 
-        n_completed = len(completed)
-        print('n_completed = {}'.format(n_completed))
+                copied = copy_back_from_scr_dir(job_dir, scr_dir, copy_back_pat)
+                n_copied = len(copied)
+                print('copied {} files from {}'.format(n_copied, last_job_id))
 
-        if args.print_indices:
-            print_array_indices(completed)
+            if args.resub_errors: # resubmit incomplete jobs
 
-        incomplete = submitted - completed
+                for m in match_files_in_dir(job_dir):
+                    job_script = os.path.join(job_dir, m.group(0))
+                    SlurmQueue.submit_job(
+                        job_file,
+                        work_dir=job_dir,
+                        array_idx=get_array_indices_string(incomplete)
+                    )
 
-        n_incomplete = len(incomplete)
-        print('n_incomplete = {}'.format(n_incomplete))
+        else:
+            job_ids = find_job_ids(job_dir, stderr_pat)
 
-        if args.print_indices:
-            print_array_indices(incomplete)
+            if args.output_file:
+                job_dfs = read_job_output(job_dir, output_pat)
+                all_job_dfs.extend(job_dfs)
 
-        if args.print_errors: # parse stderr files for incomplete indices
-            print_errors_for_array_indices(job_dir, stderr_pat, indices=incomplete)
+            if args.print_errors:
+                print_last_error(job_dir, stderr_pat)
 
-        if args.resub_errors: # resubmit incomplete jobs
+            if args.copy_back:
 
-            for m in match_files_in_dir(job_dir):
-                job_script = os.path.join(job_dir, m.group(0))
-                SlurmQueue.submit_job(
-                    job_file,
-                    work_dir=job_dir,
-                    array_idx=get_array_indices_string(incomplete)
-                )
+                for last_job_id in sorted(job_ids)[-2:]:
+                    scr_dir = os.path.join(job_dir, str(last_job_id))
+
+                    copied = copy_back_from_scr_dir(job_dir, scr_dir, copy_back_pat)
+                    n_copied = len(copied)
+                    print('copied {} files from {}'.format(n_copied, last_job_id))
 
     if args.output_file:
         if job_dfs:
