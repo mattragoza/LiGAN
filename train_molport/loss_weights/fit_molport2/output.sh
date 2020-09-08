ad3b7fb29430cba8087fca574472310e27daf2c0
Matt Ragoza <mtr22@pitt.edu>
2020-09-08 15:44:03 -0400

    update fit_molport2 from CRC loss weight sweep

--- train_molport/loss_weights/fit_molport2/output.sh
+++ train_molport/loss_weights/fit_molport2/output.sh
@@ -1,1 +1,1 @@
-python3 ../../../job_errors.py --job_type fit --array_job --submitted 1-10 $(cat all_job_dirs) --output_file crc_fit_molport2.gen_metrics
+python3 ../../../job_errors.py --job_type fit --array_job --submitted 1-100 $(cat all_job_dirs) --output_file crc_fit_molport2.gen_metrics
--- train_molport/loss_weights/fit_molport2/submit.sh
+++ train_molport/loss_weights/fit_molport2/submit.sh
@@ -3,4 +3,20 @@
 #python3 ../../../submit_job.py --array=1-10 $(cat tail_job_scripts2)
 #python3 ../../../submit_job.py --array=1-10 $(cat tail_job_scripts3)
 #python3 ../../../submit_job.py --array=1-10 $(cat tail_job_scripts4)
-python3 ../../../submit_job.py --array=1-10 $(cat tail_job_scripts5)
+#python3 ../../../submit_job.py --array=1-10 $(cat tail_job_scripts5)
+
+# begin full train evaluations
+#python3 ../../../submit_job.py --array=11-100%20 $(cat best_job_scripts)
+#python3 ../../../submit_job.py --array=11-100%20 $(cat tail_best_job_scripts)
+
+# cancelled the following jobs to try moving to gtx1080, but then moved back to titanx
+python3 ../../../submit_job.py --array=71-100%20 gen_e_0.1_1_disc_x_10_0_70000_pr/crc_fit.sh
+python3 ../../../submit_job.py --array=11-100%20 gen_e_0.1_1_disc_x_10_0_80000_pr/crc_fit.sh
+python3 ../../../submit_job.py --array=11-100%20 gen_e_0.1_1_disc_x_10_0_90000_r/crc_fit.sh
+python3 ../../../submit_job.py --array=11-100%20 gen_e_0.1_1_disc_x_10_0_90000_pr/crc_fit.sh
+python3 ../../../submit_job.py --array=11-100%20 gen_e_0.1_1_disc_x_10_0_100000_r/crc_fit.sh
+python3 ../../../submit_job.py --array=11-100%20 gen_e_0.1_1_disc_x_10_0_100000_pr/crc_fit.sh
+
+#TODO
+#python3 ../../../submit_job.py --array=101-1000%20 $(cat best_job_scripts)
+
