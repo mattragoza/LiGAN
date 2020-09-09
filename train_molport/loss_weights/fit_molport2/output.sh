8ee9f16f486faf74c357135fe30d768453e3728d
Matt Ragoza <mtr22@pitt.edu>
2020-09-09 13:59:19 -0400

    move fit_molport2 to CSB for full train evals

--- train_molport/loss_weights/fit_molport2/output.sh
+++ train_molport/loss_weights/fit_molport2/output.sh
@@ -1,1 +1,2 @@
-python3 ../../../job_errors.py --job_type fit --array_job --submitted 1-100 $(cat all_job_dirs) --output_file crc_fit_molport2.gen_metrics
+#python3 ../../../job_errors.py --job_type fit --array_job --submitted 1-100 $(cat all_job_dirs) --output_file crc_fit_molport2.gen_metrics
+python3 ../../../job_errors.py --job_type fit --array_job --submitted 1-30000 $(cat all_job_scripts | cut -d"/" -f1) --output_file csb_fit_molport2.gen_metrics
--- train_molport/loss_weights/fit_molport2/setup.sh
+++ train_molport/loss_weights/fit_molport2/setup.sh
@@ -1,2 +1,4 @@
-python3 ../../../job_scripts.py job.params -b crc_fit.sh -o . -n '{gen_model_name}_{disc_model_name}_{train_seed}_{train_iter}_{gen_options}'
+#python3 ../../../job_scripts.py vae.params -b crc_fit.sh -o . -n '{gen_model_name}_{disc_model_name}_{train_seed}_{train_iter}_{gen_options}'
+python3 ../../../job_scripts.py vae.params -b csb_fit_cmd.sh -o . -n '{gen_model_name}_{disc_model_name}_{train_seed}_{train_iter}_{gen_options}'
+python3 ../../../job_scripts.py ae.params -b csb_fit_cmd.sh -o . -n '{gen_model_name}_{disc_model_name}_{train_seed}_{train_iter}_{gen_options}'
 
