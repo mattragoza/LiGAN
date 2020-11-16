acc8fe3738f6e9b68b596254359013433da69d42
Matt Ragoza <mtr22@pitt.edu>
2020-11-16 13:33:51 -0500

    add updated loss_weights training expt scripts

--- train_molport/loss_weights/submit.sh
+++ train_molport/loss_weights/submit.sh
@@ -1,3 +1,4 @@
 #python3 ../../submit_job.py */crc_train.sh
 #python3 ../../submit_job.py $(cat unfinished_100k_job_scripts)
-python3 ../../submit_job.py $(cat unfinished_100k_job_scripts2)
+#python3 ../../submit_job.py $(cat unfinished_100k_job_scripts2)
+python3 ../../submit_job.py wgan_trial0/csb_train_expt.sh --array 1-90%10
