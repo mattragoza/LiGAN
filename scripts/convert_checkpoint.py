b55f6c5ee0ea81bc45a2a6893492537ad0e325f7
Matt Ragoza <mtr22@pitt.edu>
2021-04-04 19:05:00 -0400

    add convert_checkpoint.py and wip count_atom_types.py

---
+++ scripts/convert_checkpoint.py
@@ -0,0 +1,48 @@
+import sys, os, re, torch
+from collections import OrderedDict as odict
+
+layer_map = {
+    "input_encoder.fc0.fc.weight": "input_encoder.fc0.1.weight",
+    "input_encoder.fc0.fc.bias": "input_encoder.fc0.1.bias",
+    "input_encoder.fc1.fc.weight": "input_encoder.fc1.1.weight",
+    "input_encoder.fc1.fc.bias": "input_encoder.fc1.1.bias",
+    "conditional_encoder.fc0.fc.weight": "conditional_encoder.fc0.1.weight",
+    "conditional_encoder.fc0.fc.bias": "conditional_encoder.fc0.1.bias",
+    "decoder.fc.fc.weight": "decoder.fc.0.weight",
+    "decoder.fc.fc.bias": "decoder.fc.0.bias",
+}
+
+def stdize_gen_layers(state_dict):
+    for old_key in list(state_dict.keys()):
+        if old_key in layer_map:
+            new_key = layer_map[old_key]
+            state_dict[new_key] = state_dict.pop(old_key)
+    return state_dict
+
+
+for in_file in sys.argv[1:]:
+
+    prefix = os.path.splitext(in_file)[0]
+    curr_iter = int(re.match(r'.*_iter_(\d+)', prefix).group(1))
+    d = torch.load(in_file)
+
+    save = lambda o,f: (print(f) or torch.save(o, f))
+
+    save(
+        stdize_gen_layers(d['gen_model_state']),
+        prefix + '.gen_model_state'
+    )
+    save(odict([
+        ('optim_state', stdize_gen_layers(d['gen_optim_state'])),
+        ('iter', curr_iter),
+    ]), prefix + '.gen_solver_state')
+
+    save(
+        d['disc_model_state'],
+        prefix + '.disc_model_state'
+    )
+    save(odict([
+        ('optim_state', d['disc_optim_state']),
+        ('iter', curr_iter),
+    ]), prefix + '.disc_solver_state')
+
--- scripts/count_atom_types.py
+++ scripts/count_atom_types.py
@@ -1,26 +1,40 @@
-import sys, os
-from collections import Counter
+import sys, molgrid
+import numpy as np
+sys.path.insert(0, '.')
+import liGAN
 
-from atom_types import get_default_lig_channels
-from generate import read_gninatypes_file
+rec_typer = molgrid.FileMappedGninaTyper('data/my_rec_map')
+lig_typer = molgrid.FileMappedGninaTyper('data/my_lig_map')
+lig_channels = liGAN.atom_types.get_channels_from_map(lig_typer)
 
-channels = get_default_lig_channels(False)
-data_file, data_root = sys.argv[1:]
+print('loading data')
+ex_provider = molgrid.ExampleProvider(
+	rec_typer,
+	lig_typer,
+	data_root='data/molport',
+	recmolcache='data/molportFULL_rec.molcache2' and '',
+	ligmolcache='data/molportFULL_lig.molcache2' and '',
+	shuffle=True
+)
+ex_provider.populate('data/molportFULL_rand_test0_1000.types')
 
-with open(data_file, 'r') as f:
-    lines = f.readlines()
+batch_size = 1000
+n_examples = ex_provider.size()
+n_batches = n_examples // batch_size
 
-cnt = Counter()
-for line in lines:
-    fields = line.rstrip().split()
-    lig_file = os.path.join(data_root, fields[3])
-    xyz, c = read_gninatypes_file(lig_file, channels)
-    cnt.update(c)
+type_counts = np.zeros(lig_typer.num_types())
+mol_count = 0
 
-n = 0
-for i, channel in enumerate(channels):
-    print('{} {} {}'.format(i, channel.name, cnt[i]))
-    n += cnt[i]
+for i in range(n_batches):
+	for ex in ex_provider.next_batch(batch_size):
+		struct = liGAN.atom_structs.AtomStruct.from_coord_set(
+			ex.coord_sets[1], lig_channels
+		)
+		type_counts += struct.type_counts
+		mol_count += 1
+	print('batch {}/{}'.format(i+1, n_batches))
 
-print('{} total'.format(n))
-
+for i, channel in enumerate(lig_channels):
+    print('{:2d} {:10d} {:8.3f} {}'.format(
+    	i, int(type_counts[i]), type_counts[i]/mol_count, channel.name
+    ))
