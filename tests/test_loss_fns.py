042b54fc996c1ddabf806e064865718d3fa60416
Matt Ragoza <mtr22@pitt.edu>
2021-07-08 19:58:30 -0400

    implement cosine loss weight scheduling

--- tests/test_loss_fns.py
+++ tests/test_loss_fns.py
@@ -4,6 +4,7 @@ from torch import optim
 
 sys.path.insert(0, '.')
 import liGAN
+from liGAN.loss_fns import LossFunction
 
 
 class TestLossFunction(object):
@@ -17,7 +18,7 @@ class TestLossFunction(object):
 
     @pytest.fixture
     def loss_fn(self, loss_kws):
-        return liGAN.loss_fns.LossFunction(device='cpu', **loss_kws)
+        return LossFunction(device='cpu', **loss_kws)
 
     def test_loss_init(self, loss_fn):
         assert loss_fn.kldiv_loss_wt == 0.0
@@ -37,7 +38,8 @@ class TestLossFunction(object):
             lig_grids=lig_grids, lig_gen_grids=lig_gen_grids
         )
         assert loss.item() == 0
-        assert losses == dict(loss=0, recon_loss=0)
+        print(losses)
+        assert losses == dict(loss=0, recon_loss=0, recon_loss_wt=1)
 
     def test_loss_recon1(self, loss_fn):
         lig_grids = torch.zeros(10, 10)
@@ -46,7 +48,8 @@ class TestLossFunction(object):
             lig_grids=lig_grids, lig_gen_grids=lig_gen_grids
         )
         assert loss.item() == 5
-        assert losses == dict(loss=5, recon_loss=5)
+        print(losses)
+        assert losses == dict(loss=5, recon_loss=5, recon_loss_wt=1)
 
     def test_loss_kldiv0(self, loss_fn):
         a = torch.zeros(10, 10)
@@ -55,7 +58,7 @@ class TestLossFunction(object):
             latent_means=a, latent_log_stds=b
         )
         assert loss.item() == 0
-        assert losses == dict(loss=0, kldiv_loss=0)
+        assert losses == dict(loss=0, kldiv_loss=0, kldiv_loss_wt=0)
 
     def test_loss_kldiv1(self, loss_fn):
         a = torch.ones(10, 10)
@@ -64,7 +67,7 @@ class TestLossFunction(object):
             latent_means=a, latent_log_stds=b
         )
         assert loss.item() == 0
-        assert losses == dict(loss=0, kldiv_loss=5)
+        assert losses == dict(loss=0, kldiv_loss=5, kldiv_loss_wt=0)
 
     def test_loss_gan0(self, loss_fn):
         a = torch.zeros(10, 10)
@@ -73,7 +76,7 @@ class TestLossFunction(object):
             disc_labels=a, disc_preds=b
         )
         assert loss.item() == 0
-        assert losses == dict(loss=0, gan_loss=0)
+        assert losses == dict(loss=0, gan_loss=0, gan_loss_wt=10)
 
     def test_loss_gan1(self, loss_fn):
         a = torch.ones(10, 10)
@@ -82,7 +85,8 @@ class TestLossFunction(object):
             disc_labels=a, disc_preds=b
         )
         assert loss.item() == 100
-        assert losses == dict(loss=100, gan_loss=10)
+        print(losses)
+        assert losses == dict(loss=100, gan_loss=10, gan_loss_wt=10)
 
     def test_loss_steric0(self, loss_fn):
         a = torch.zeros(10, 10)
@@ -91,7 +95,7 @@ class TestLossFunction(object):
             rec_grids=a, rec_lig_grids=b
         )
         assert loss.item() == 0
-        assert losses == dict(loss=0, steric_loss=0)
+        assert losses == dict(loss=0, steric_loss=0, steric_loss_wt=-1)
 
     def test_loss_steric1(self, loss_fn):
         a = torch.ones(10, 10)
@@ -100,4 +104,17 @@ class TestLossFunction(object):
             rec_grids=a, rec_lig_grids=b
         )
         assert loss.item() == -100
-        assert losses == dict(loss=-100, steric_loss=100)
+        assert losses == dict(loss=-100, steric_loss=100, steric_loss_wt=-1)
+
+
+class TestLossSchedule(object):
+
+    @pytest.fixture
+    def iters(self):
+        return range(1, 1000001, 1000)
+
+    def test_schedule_0(self, iters):
+
+        f = liGAN.loss_fns.get_loss_schedule(
+            start_wt=1, end_wt=1
+        )
